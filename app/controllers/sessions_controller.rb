87cbf9a5b906a5095e1fd77cf18c5c9c84e66a39
Ryan Dorr <ryandorr17@gmail.com>
2017-10-21 16:09:59 -0500

    fix up home and users page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
 
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 
-        redirect_back_or user
+        redirect_back_or users_path
 
       else
 
