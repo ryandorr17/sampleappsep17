44f8ba854d494e95a5b03a50e874b6cb836e7a44
Ryan Dorr <ryandorr17@gmail.com>
2017-10-28 07:33:43 -0500

    add the fucking comma

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -10,7 +10,7 @@ if Rails.env.production?
 
       :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
 
-      :aws_secret_access_key  => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
 
       :region => ENV['S3_REGION']
 
