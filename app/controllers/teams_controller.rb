fe318bbcf88952c86bcbbe2b79430f4e7bcfef2f
Ryan Dorr <ryandorr17@gmail.com>
2017-10-28 07:32:51 -0500

    update carrier wave region

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -53,3 +53,4 @@ private
 
 
 end
+
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -12,6 +12,8 @@ if Rails.env.production?
 
       :aws_secret_access_key  => ENV['S3_SECRET_KEY']
 
+      :region => ENV['S3_REGION']
+
     }
 
     config.fog_directory       = ENV['S3_BUCKET']
